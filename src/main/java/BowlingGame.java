17385be9707eb1666bd3eec05f2ac61f8d0fcc3c
yi-dai <851758415@qq.com>
2020-08-03 15:09:14 +0800

    [yi dai] implements strike frame in top nine frame.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -5,17 +5,22 @@ public class BowlingGame {
         int result = 0;
         int pre = 0;
         for (int i = 0; i < bowling_throw_score.size(); i ++){
+            int now = bowling_throw_score.get(i);
+            if (now == 10){
+                result = result + now + bowling_throw_score.get(i+1) + bowling_throw_score.get(i+2);
+                bowling_throw_score.add(i+1,0);
+                continue;
+            }
             if (i % 2 == 1){
-                if (pre + bowling_throw_score.get(i) == 10) {
-                    result = bowling_throw_score.get(i) + result + bowling_throw_score.get(i + 1);
+                if (pre + now == 10) {
+                    result = now + result + bowling_throw_score.get(i+1);
                 } else{
-                    result = bowling_throw_score.get(i) + result;
+                    result = now + result;
                 }
             } else {
-                pre = bowling_throw_score.get(i);
-                result = bowling_throw_score.get(i) + result;
+                pre = now;
+                result = now + result;
             }
-                //result = bowling_throw_score.get(i) + result;
         }
         return result;
     }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -33,4 +33,32 @@ public class BowlingGameTest {
         //then
         assertEquals(expect,final_score);
     }
+
+    @Test
+    void some_strike_frame_in_top_nine_frame(){
+        //given
+        List<Integer> bowling_throw_score = new ArrayList<Integer>();
+        Collections.addAll(bowling_throw_score, 2, 5, 4, 6, 4, 5, 1, 9, 2, 5, 5, 3, 4, 3, 6, 3, 10, 2, 5);
+        //int[] bowling_throw_score = {2, 5, 3, 6, 4, 5, 1, 7, 2, 5, 5, 3, 4, 3, 6, 3, 3, 3, 2, 5};
+        int expect = 97;
+        //when
+        BowlingGame bowlingGame = new BowlingGame();
+        int final_score = bowlingGame.convert(bowling_throw_score);
+        //then
+        assertEquals(expect,final_score);
+    }
+
+    //@Test
+    void spare_frame_in_last_frame(){
+        //given
+        List<Integer> bowling_throw_score = new ArrayList<Integer>();
+        Collections.addAll(bowling_throw_score, 2, 5, 4, 6, 4, 5, 1, 9, 2, 5, 5, 3, 4, 3, 6, 3, 3, 3, 2, 5);
+        //int[] bowling_throw_score = {2, 5, 3, 6, 4, 5, 1, 7, 2, 5, 5, 3, 4, 3, 6, 3, 3, 3, 2, 5};
+        int expect = 86;
+        //when
+        BowlingGame bowlingGame = new BowlingGame();
+        int final_score = bowlingGame.convert(bowling_throw_score);
+        //then
+        assertEquals(expect,final_score);
+    }
 }
